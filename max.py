7e227e366eee4df4cf41928ce95882794f90c62f
g170031197 <59306994+g170031197@users.noreply.github.com>
2019-12-28 15:15:14 +0530

    Add files via upload

--- max.py
+++ max.py
@@ -1,14 +1,17 @@
-def largest(arr,n): 
-  
-    
-    max = arr[0] 
-  
-   for i in range(1, n): 
-        if arr[i] > max: 
-            max = arr[i] 
-    return max
-  
-arr = [10, 324, 45, 90, 9808] 
-n = len(arr) 
-Ans = largest(arr,n) 
-print ("Largest in given array is",Ans) 
+# Python 3 program to find
+# maximum in arr[] of size n
+
+# returns maximum
+# in arr[] of size n
+def largest(arr, n):
+
+return max(arr)
+
+# driver code
+arr = [10, 324, 45, 90, 9808]
+n = len(arr)
+
+print(largest(arr, n))
+
+# This code is contributed by
+# Smitha Dinesh Semwal
